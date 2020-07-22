69a3f5c4e4fbe5e3027f2d0d5133e7e8e8eace2b
Danucas <danrodcastillo1994@gmail.com>
2020-07-22 00:37:02 -0500

    split fixed, using \n as patter

--- read.py
+++ read.py
@@ -1,7 +1,15 @@
 #/usr/bin/python3
-#Read .obj .txt files an parsed to objects
+"""
+Read .obj .txt files an parsed to objects
+"""
 
 def obj_read(filename):
+    """
+    Open file from obj folder as read
+    and create vertex objects with the data
+    return a dict with vertex and faces
+    to render the isometric map
+    """
     content = None
     scale = 1
     with open(filename, "r") as f:
@@ -11,7 +19,10 @@ def obj_read(filename):
     for line in content:
         line = line.split(" ")
         if line[0] == "v":
-            vertex.append({"x": float(line[1]) * scale, "y": float(line[2]) * scale, "z": float(line[3]) * scale})
+            vertex.append({
+                "x": float(line[1]) * scale,
+                "y": float(line[2]) * scale,
+                "z": float(line[3]) * scale})
         if line[0] == "f":
             del line[0]
             face = []
@@ -20,5 +31,6 @@ def obj_read(filename):
                 face.append(int(con_point[0]) - 1)
             face.append(int(line[0].split("/")[0]) - 1)
             faces.append(face)
-        #print(vertex, faces)
-    return ({"vertex": list(reversed(vertex)),"faces": faces})
+    return ({
+        "vertex": list(reversed(vertex)),
+        "faces": faces})
